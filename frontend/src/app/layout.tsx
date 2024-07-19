97d28d38fddddc1c5fe340e0c3beff48a18e234a
kazakhpunk <nursultansagyntay.1@gmail.com>
2024-07-19 08:47:23 +0500

    Changed Metadata

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -7,8 +7,8 @@ import { ThemeProvider } from "@/providers/theme-provider";
 import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
-  title: 'Website Title',
-  description: 'Website description',
+  title: 'Quant Trader AI',
+  description: "Quant Trader AI is an advanced trading platform that leverages artificial intelligence to provide real-time trading signals. The platform includes detailed fundamental analysis to offer insights into a company's financial health, technical analysis to predict price movements, volatility analysis to manage risks associated with price changes, and sentiment analysis to gauge market sentiment from news and social media. Our aim is to empower traders to make informed decisions by automating the process of buying low and selling high.",
   icons: {
     icon: [
       {
