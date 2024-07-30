6d2eff9c2d34942da5e0fc8a623f20695927b371
kazakhpunk <nursultansagyntay.1@gmail.com>
2024-07-30 17:05:35 +0500

    Added Analytics

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { GeistSans } from "geist/font/sans";
+import { Analytics } from "@vercel/analytics/react";
 
 import "./globals.css";
 
@@ -7,26 +8,27 @@ import { ThemeProvider } from "@/providers/theme-provider";
 import { ClerkProvider } from "@clerk/nextjs";
 
 export const metadata: Metadata = {
-  title: 'Quant Trader AI',
-  description: "Quant Trader AI is an advanced trading platform that leverages artificial intelligence to provide real-time trading signals. The platform includes detailed fundamental analysis to offer insights into a company's financial health, technical analysis to predict price movements, volatility analysis to manage risks associated with price changes, and sentiment analysis to gauge market sentiment from news and social media. Our aim is to empower traders to make informed decisions by automating the process of buying low and selling high.",
+  title: "Quant Trader AI",
+  description:
+    "Quant Trader AI is an advanced trading platform that leverages artificial intelligence to provide real-time trading signals. The platform includes detailed fundamental analysis to offer insights into a company's financial health, technical analysis to predict price movements, volatility analysis to manage risks associated with price changes, and sentiment analysis to gauge market sentiment from news and social media. Our aim is to empower traders to make informed decisions by automating the process of buying low and selling high.",
   icons: {
     icon: [
       {
-        media: '(prefers-color-scheme: light)',
-        url: '/gem.ico',
-        href: '/gem.ico',
+        media: "(prefers-color-scheme: light)",
+        url: "/gem.ico",
+        href: "/gem.ico",
       },
       {
-        media: '(prefers-color-scheme: dark)',
-        url: '/gem.ico',
-        href: '/gem.ico',
+        media: "(prefers-color-scheme: dark)",
+        url: "/gem.ico",
+        href: "/gem.ico",
       },
     ],
   },
 };
 
 export default function RootLayout({
-  children
+  children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
@@ -36,6 +38,7 @@ export default function RootLayout({
         <body className={GeistSans.className}>
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             {children}
+            <Analytics />
           </ThemeProvider>
         </body>
       </html>
