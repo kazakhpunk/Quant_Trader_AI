eb8fa94997d177e07122626518f2f793f259a74c
kazakhpunk <nursultansagyntay.1@gmail.com>
2024-08-08 03:43:00 +0500

    Changed link path

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -56,7 +56,7 @@ export default function HomePage() {
             </span>
             <div className="flex w-full items-center justify-center space-x-4 py-4 md:pb-6">
               <Button variant="default" asChild>
-                <Link href="/dashboard">
+                <Link href="/trade">
                   Start Trading
                   <ArrowRightIcon className="ml-2" />
                 </Link>
